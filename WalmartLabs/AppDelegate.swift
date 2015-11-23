c2fb85d3bd8d7ebbd5e31107fdc6941ef2a85d33
Ayush Chamoli <achamol@searshc.com>
2015-11-23 15:51:33 -0600

    Disable Caching.

--- WalmartLabs/AppDelegate.swift
+++ WalmartLabs/AppDelegate.swift
@@ -20,26 +20,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(application: UIApplication)
+    {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(application: UIApplication)
     {
-        self.saveLoadedProducts()
+        //self.saveLoadedProducts()
     }
 
     func applicationWillEnterForeground(application: UIApplication)
     {
-        self.retrieveLoadedProducts()
+        //self.retrieveLoadedProducts()
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(application: UIApplication)
+    {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(application: UIApplication)
+    {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
